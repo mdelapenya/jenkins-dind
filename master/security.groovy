048393a43b8d57edfa4ef82b2f990db78360b1c6
Manuel de la Peña <manuel.delapenya@liferay.com>
2018-06-11 11:37:42 +0200

    Add pipelines signatures to Jenkins in-script approvals

--- master/security.groovy
+++ master/security.groovy
@@ -3,7 +3,8 @@
 import jenkins.model.*
 import hudson.security.*
 import jenkins.security.s2m.AdminWhitelistRule
- 
+import org.jenkinsci.plugins.scriptsecurity.scripts.*
+
 def instance = Jenkins.getInstance()
  
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
@@ -15,3 +16,19 @@ instance.setAuthorizationStrategy(strategy)
 instance.save()
  
 Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
+
+ScriptApproval sa = ScriptApproval.get(); 
+
+def signature = "new net.sf.json.JSONObject"
+sa.getPendingSignatures().add(new ScriptApproval.PendingSignature(signature, false, ApprovalContext.create()))
+
+signature = "new net.sf.json.JSONArray"
+sa.getPendingSignatures().add(new ScriptApproval.PendingSignature(signature, false, ApprovalContext.create()))
+
+// approve signatures
+for (ScriptApproval.PendingSignature pending : sa.getPendingSignatures()) {
+   if (pending.equals(signature)) {
+       	sa.approveSignature(pending.signature);
+     	println "Approved : " + pending.signature
+      }
+}
